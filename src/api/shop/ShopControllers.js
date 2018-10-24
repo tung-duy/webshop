6dc064b05818ca7e445d53b2f13c8b9a28de8eb2
Tung Duy <daoduytung9x@gmail.com>
2018-10-24 12:36:32 +0700

    Update category
    I've finished cateogry update feature

--- src/api/shop/ShopControllers.js
+++ src/api/shop/ShopControllers.js
@@ -1,4 +1,6 @@
+const Sequelize = require("sequelize");
 const { Categories } = require("./ShopModel");
+const Op = Sequelize.Op;
 
 module.exports = {
   addCategory: async (req, res) => {
@@ -13,5 +15,25 @@ module.exports = {
     Categories.create(newCate)
       .then(cate => res.json(cate))
       .catch(err => console.log(err));
+  },
+  updateCategory: async (req, res) => {
+    const errors = {};
+    const id = req.params.cate_id;
+    const { name, description } = req.body;
+
+    const isNameUnique = await Categories.findOne({
+      where: {
+        [Op.and]: [{ [Op.not]: { id } }, { name }]
+      }
+    });
+
+    if (isNameUnique) {
+      errors.name = "Name already exists!";
+      return res.status(404).json(errors);
+    }
+    const cate = await Categories.findById(id);
+    cate.update({ name, description }).then(cate => {
+      return res.json(cate);
+    });
   }
 };
--- src/api/shop/routers.js
+++ src/api/shop/routers.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const ProductControllers = require("./ShopControllers");
+const ShopControllers = require("./ShopControllers");
 const passport = require("passport");
 
 router.post(
   "/cate/add",
   passport.authenticate("jwt", { session: false }),
-  ProductControllers.addCategory
+  ShopControllers.addCategory
 );
-
+router.post(
+  "/cate/update/:cate_id",
+  passport.authenticate("jwt", { session: false }),
+  ShopControllers.updateCategory
+);
 // @route       api/shop
 module.exports = router;
